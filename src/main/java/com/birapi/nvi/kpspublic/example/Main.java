7caafc0076af53b08c8ff327933706f94e50bc6a
Mehmet Gurevin <mehmet@gurevin.net>
2022-02-16 23:50:36 +0300

    Update Main.java

--- src/main/java/com/birapi/nvi/kpspublic/example/Main.java
+++ src/main/java/com/birapi/nvi/kpspublic/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		KPSPublicSoap client = new KPSPublic().getKPSPublicSoap();
 
-		Long tckn = 10999275068;
+		Long tckn = 10999275068l;
 		String firstName = "name";
 		String lastName = "surname";
 		int birthYear = 1985;
